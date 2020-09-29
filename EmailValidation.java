b8a32332ef8b36580eca419b9eae2e2fc58bd479
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-29 12:37:32 +0530

    [Gaurav]REFACTORED: Validating first mandatory part in email

--- EmailValidation.java
+++ EmailValidation.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidation
 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Email Validation Program");
+
+		/* In Email i.e abc.xyz@bridgelabz.co.in --> abc,bridgelabz and co are mandatory parts, .xyz and in are optional and @ and . are mandatory respectively*/
+		
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter the first mandatory part to be validated");
+		String patternToBeValidated = scan.nextLine();
+
+		Pattern pattern = Pattern.compile("[a-zA-Z0-9]+");
+		Matcher match = pattern.matcher(patternToBeValidated);
+		
+		if(match.matches())
+		{
+			System.out.println("Pattern is valid");
+		}
+		else
+		{
+			System.out.println("Pattern is Invalid");
+		}
 	}
 }
